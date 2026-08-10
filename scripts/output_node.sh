e437b3db1af66836953e8a7e25ce9fe225b2c9b8
rpengwg <105784675+rpengwg@users.noreply.github.com>
2026-08-10 06:47:36 -0400

    Update output_node.sh

--- scripts/output_node.sh
+++ scripts/output_node.sh
@@ -12,38 +12,44 @@ echo "VLESS TCP Reality"
 
 echo
 
-echo "UUID:"
-echo "${UUID}"
+echo "Address:"
 
-
-echo
-
-echo "Server:"
-echo "${RAILWAY_PUBLIC_DOMAIN}"
+echo "${RAILWAY_TCP_PROXY_DOMAIN}"
 
 
 echo
 
 echo "Port:"
-echo "${PORT}"
+
+echo "${RAILWAY_TCP_PROXY_PORT}"
 
 
 echo
 
-echo "SNI:"
-echo "${SNI}"
+echo "UUID:"
+
+echo "${UUID}"
 
 
 echo
 
 echo "PublicKey:"
+
 echo "${PUBLIC_KEY}"
 
 
 echo
 
 echo "ShortID:"
+
 echo "${SHORT_ID}"
+
+
+echo
+
+echo "SNI:"
+
+echo "${SNI}"
 
 
 echo
